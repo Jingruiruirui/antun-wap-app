d489059a6d9d19c23bcdff165b76960d9b632a09
Jingruiruirui <Jingruiruirui@126.com>
2017-11-29 15:34:22 +0800

    修改common.js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
 // server = 'http://47.94.250.65/antun';
-server = 'http://localhost:9092';
+//server = 'http://localhost:9092';
+server='http://192.168.12.211/antun';
+
+
 url_user=server+"/users";
 url_area=server+"/areas";
 url_link = server + "/links";
